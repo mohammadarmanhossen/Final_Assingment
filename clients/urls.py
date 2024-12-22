576ee1b391eb39510104c8cad9b46137f29eb2dc
arman <mdarmanhossen404@gmail.com>
2024-12-22 23:02:00 +0600

    first commit

---
+++ clients/urls.py
@@ -0,0 +1,15 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path,include
+from . import views
+router=DefaultRouter()
+router.register('list',views.ClientViewSet),
+router.register('contact',views.ContactViewSet),
+router.register('deposit',views.DepositViewSet),
+
+urlpatterns = [
+    path('',include(router.urls)),
+    path('register/',views.UserRegistrationApiView.as_view(),name='register'),
+    path('login/',views.UserLoginApiView.as_view(),name='login'),
+    path('logout/', views.UserLogoutApiView.as_view(),name='logout'),
+    path('active/<uid64>/<token>',views.activate,name='activate'),
+]
