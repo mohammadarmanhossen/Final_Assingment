bbb8b9f04c7bb4cb844a7a616277480e0b269f7b
arman <mdarmanhossen404@gmail.com>
2025-01-03 11:14:36 +0600

    Final Assingment Dajngo

--- hotels/views.py
+++ hotels/views.py
@@ -4,7 +4,6 @@ from rest_framework import viewsets
 from .import models
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
-from rest_framework import pagination
 from .import serializer
 # Create your views here.
 
@@ -16,10 +15,6 @@ class DistrictViewSet(viewsets.ModelViewSet):
     serializer_class=serializer.DistrictSerializer
     lookup_field = 'slug'
 
-class HotelPagination(pagination.PageNumberPagination):
-    page_size=1
-    page_size_query_param ="page_size"
-    max_page_size=100
 
 class HotelViewSet(viewsets.ModelViewSet):
     queryset=models.Hotel.objects.all()
@@ -27,7 +22,6 @@ class HotelViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['district_names']  # Fields for exact filtering
     search_fields = ['hotel_name']  # Fields for partial search
-    pagination_class = HotelPagination
 
 
 
